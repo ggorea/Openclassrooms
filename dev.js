5cce71c75a99b2f1e24ad3cbcbd6d66a84cecc88
ggorea <georgeta05@mail.ru>
2016-01-15 20:09:26 +0100

    Cette ligne génère un nombre entre 1 et 100

--- dev.js
+++ dev.js
@@ -2,3 +2,5 @@
 Activité : jeu de devinette
 */
 console.log("Bienvenue dans ce jeu de devinette!")
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+var solution = Math.floor(Math.random() * 100) + 1;
